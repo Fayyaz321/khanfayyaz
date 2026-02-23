3fd9754a9aa5ee2668abd5f583d2579ea28ea1c4
Fayyaz <khanfayyaz736@gmail.com>
2026-02-23 19:41:59 +0500

    README.md کو اردو میں اپڈیٹ کیا

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-//  var elemC = document.querySelector(".elem-container")
-// var fixed = document.querySelector(".fixed-image")
-// var elems = document.querySelectorAll(".elem")
-
-// elemC.addEventListener("mouseenter", function () {
-//   fixed.style.display = "block"
-// })
-
-// elemC.addEventListener("mouseleave", function () {
-//   fixed.style.display = "none"
-// })
-
-// elems.forEach(function (e) {
-//   e.addEventListener("mouseenter", function () {
-//     var image = e.getAttribute("data-image")
-//     fixed.style.backgroundImage = `url("${image}")`
-//   })
-// })
-
-// window.addEventListener("load", () => {
-//   const loader = document.querySelector(".loader");
-
-//   setTimeout(() => {
-//     loader.style.top = "-100%"
-//   },4500); // loader کا total time
-// });
-
 var elemC = document.querySelector(".elem-container");
 var fixed = document.querySelector(".fixed-image");
 var elems = document.querySelectorAll(".elem");
